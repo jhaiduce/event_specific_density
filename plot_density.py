0adbded9fea39902b839f361ff575c39fdb5acf2
John D. Haiducek <jhaiduce@florence.lanl.gov>
2014-06-25 14:50:22 -0600

    Added latitude dependence using the expression from the Ozhogin density model, and added plots of the effects of density and MLT.

--- plot_density.py
+++ plot_density.py
@@ -25,14 +25,17 @@ def sheeley_uncertainty_plasmasphere(L):
 def sheeley_uncertainty_trough(L,LT):
     return 78*(3.0/L)**4.72+17*(3.0/L)**3.75*np.cos((LT-22)*np.pi/12)
 
-def ozhogin_density_eq(L):
+def ozhogin_density_equator(L):
     return 10**(4.4693-0.4903)*L
 
-def ozhogin_uncertainty_eq(L,sign):
+def ozhogin_uncertainty_equator(L,sign):
     return 10**(4.4693+sign*0.0921-(0.4903+sign*0.0315)*L)-ozhogin_density_eq(L)
 
 def ozhogin_density_latitude_factor(lat,lat_inv):
     return np.cos(np.pi/2*1.01*lat/lat_inv)**-0.75
+
+def ozhogin_latitude_factor_uncertainty(lat,lat_inv,sign):
+    return np.cos(np.pi/2*(1.01+sign*0.03)*lat/lat_inv)**-(0.75+sign*0.08)-ozhogin_density_latitude_factor(lat,lat_inv)
 
 def smoothstep(edge0,edge1,x):
     x=np.clip((x-edge0)/(edge1-edge0),0,1)
@@ -42,7 +45,7 @@ def smootherstep(edge0,edge1,x):
     x=np.clip((x-edge0)/(edge1-edge0),0,1)
     return x*x*x*(x*(x*6-15)+10)
 
-def fitdensity(L,MLT,pL,pW,ps,ts):
+def fitdensity(L,MLT,MLAT,InvLat,pL,pW,ps,ts):
     """
     A plasmapause fit function based on the Sheeley model for the trough and plasmasphere
 
@@ -59,21 +62,28 @@ def fitdensity(L,MLT,pL,pW,ps,ts):
 
     w=smoothstep(pL-pW/2,pL+pW/2,L)
 
-    return tDens*ts*w + pDens*ps*(1-w)
+    return (tDens*ts*w + pDens*ps*(1-w))*ozhogin_density_latitude_factor(MLAT,InvLat)
 
-def fituncert(L,MLT,pL,pW,ps,ts):
+def fituncert(L,MLT,MLAT,InvLat,pL,pW,ps,ts,sign):
 
+    # Sheeley uncertainty
     pUncert=sheeley_uncertainty_plasmasphere(L)
     tUncert=sheeley_uncertainty_trough(L,MLT)
 
+    # Sheeley density
+    pDens=sheeley_density_plasmasphere(L)
+    tDens=sheeley_density_trough(L,MLT)
+
+    # Plasmapause transition factor
     w=smoothstep(pL-pW/2,pL+pW/2,L)
 
-    return tUncert*ts*w + pUncert*ps*(1-w)
+    # Total uncertainty
+    return ((tUncert*ts*w + pUncert*ps*(1-w))/(tDens*ts*w + pDens*ps*(1-w))+ozhogin_latitude_factor_uncertainty(MLAT,InvLat,sign)/ozhogin_density_latitude_factor(MLAT,InvLat))*(tDens*ts*w + pDens*ps*(1-w))*ozhogin_density_latitude_factor(MLAT,InvLat)
     
 
-def fitfunc(x,L,MLT,meas_dens):
+def fitfunc(x,L,MLT,MLAT,InvLat,meas_dens):
     pL,pW,ps,ts=x
-    return np.log(fitdensity(L,MLT,pL,pW,ps,ts))-np.log(meas_dens)
+    return np.log(fitdensity(L,MLT,MLAT,InvLat,pL,pW,ps,ts))-np.log(meas_dens)
 
 def get_density_and_time(scname,dstart,dend):
     times,density=emfisis.get_data(scname,['Epoch','density'],dstart,dend)
@@ -82,10 +92,11 @@ def get_density_and_time(scname,dstart,dend):
     Lsint=ephem.get_interpolator('Lstar')
     MLTint=ephem.get_interpolator('EDMAG_MLT')
     MLATint=ephem.get_interpolator('EDMAG_MLAT')
+    InvLatint=ephem.get_interpolator('InvLat')
     otimes=date2num(times)
-    return times,Lsint(otimes),MLTint(otimes),density
+    return times,Lsint(otimes),MLTint(otimes),MLATint(otimes),InvLatint(otimes),density
 
-times,Lstar,MLT,density=get_density_and_time('rbspa',datetime(2012,10,6),datetime(2012,10,9))
+times,Lstar,MLT,MLAT,InvLat,density=get_density_and_time('rbspa',datetime(2012,10,6),datetime(2012,10,9))
 
 # Find the points that are valid in all arrays
 validpoints=np.where(-(density.mask+times.mask))
@@ -94,6 +105,8 @@ validpoints=np.where(-(density.mask+times.mask))
 times=times[validpoints]
 Lstar=Lstar[validpoints]
 MLT=MLT[validpoints]
+MLAT=MLAT[validpoints]
+InvLat=InvLat[validpoints]
 density=density[validpoints]
 
 def local_maxima(a):
@@ -122,28 +135,29 @@ for i in range(len(segmentbounds)-1):
     Lseg=Lstar[segmentbounds[i]:segmentbounds[i+1]]
     dseg=density[segmentbounds[i]:segmentbounds[i+1]]
     MLTseg=MLT[segmentbounds[i]:segmentbounds[i+1]]
+    MLATseg=MLAT[segmentbounds[i]:segmentbounds[i+1]]
+    InvLatseg=InvLat[segmentbounds[i]:segmentbounds[i+1]]
     tseg=times[segmentbounds[i]:segmentbounds[i+1]]
     fig1.gca().plot(tseg,dseg,linestyle='',marker='.',color=colors[i%len(colors)])
     fig2.gca().plot(Lseg,dseg,linestyle='',marker='.',color=colors[i%len(colors)])
 
-    fitresult = leastsq(fitfunc,[3.6,0.8,1,1],args=(Lseg,MLTseg,dseg),
+    fitresult = leastsq(fitfunc,[3.6,0.8,1,1],args=(Lseg,MLTseg,MLATseg,InvLatseg,dseg),
                         maxfev=10000,full_output=True,ftol=1e-4,xtol=1e-4)
     #print fitresult[2]['nfev']
     pL,pW,ps,ts=fitresult[0]
     fitcoeffs[i,:]=(date2num(tseg[0]),date2num(tseg[-1]),pL,pW,ps,ts)
 
-    fig1.gca().plot(tseg,fitdensity(Lseg,MLTseg,pL,pW,ps,ts),color='k')
-    fig1.gca().plot(tseg,fitdensity(Lseg,MLTseg,pL,pW,ps,ts)+fituncert(Lseg,MLTseg,pL,pW,ps,ts),color='k',linestyle=':')
-    fig1.gca().plot(tseg,fitdensity(Lseg,MLTseg,pL,pW,ps,ts)-fituncert(Lseg,MLTseg,pL,pW,ps,ts),color='k',linestyle=':')
-    fig2.gca().plot(Lseg,fitdensity(Lseg,MLTseg,pL,pW,ps,ts),color='k')
-    fig2.gca().plot(Lseg,fitdensity(Lseg,MLTseg,pL,pW,ps,ts)+fituncert(Lseg,MLTseg,pL,pW,ps,ts),color='k',linestyle=':')
-    fig2.gca().plot(Lseg,fitdensity(Lseg,MLTseg,pL,pW,ps,ts)-fituncert(Lseg,MLTseg,pL,pW,ps,ts),color='k',linestyle=':')
+    fig1.gca().plot(tseg,fitdensity(Lseg,MLTseg,MLATseg,InvLatseg,pL,pW,ps,ts),color='k')
+    fig1.gca().plot(tseg,fitdensity(Lseg,MLTseg,MLATseg,InvLatseg,pL,pW,ps,ts)+fituncert(Lseg,MLTseg,MLATseg,InvLatseg,pL,pW,ps,ts,1),color='k',linestyle=':')
+    fig1.gca().plot(tseg,fitdensity(Lseg,MLTseg,MLATseg,InvLatseg,pL,pW,ps,ts)-fituncert(Lseg,MLTseg,MLATseg,InvLatseg,pL,pW,ps,ts,-1),color='k',linestyle=':')
+    fig2.gca().plot(Lseg,fitdensity(Lseg,MLTseg,MLATseg,InvLatseg,pL,pW,ps,ts),color='k')
+    fig2.gca().plot(Lseg,fitdensity(Lseg,MLTseg,MLATseg,InvLatseg,pL,pW,ps,ts)+fituncert(Lseg,MLTseg,MLATseg,InvLatseg,pL,pW,ps,ts,1),color='k',linestyle=':')
+    fig2.gca().plot(Lseg,fitdensity(Lseg,MLTseg,MLATseg,InvLatseg,pL,pW,ps,ts)-fituncert(Lseg,MLTseg,MLATseg,InvLatseg,pL,pW,ps,ts,-1),color='k',linestyle=':')
 
     fig2.gca().set_yscale('log')
     fig2.gca().set_ylim(1e0,1e4)
     fig2.gca().set_ylabel(r'Electron density (cm^-3)')
     fig2.gca().set_xlabel('L (Re)')
-
 
 fh=open("fitcoeffs.csv",'w')
 fh.write("Interval start (ordinal days), Interval end(ordinal days), Plasmapause L* (Re), Plasmapause width (Re), Plasmasphere multiplier, Trough multiplier")
@@ -157,5 +171,22 @@ fig1.gca().set_yscale('log')
 fig1.gca().set_ylim(1e0,1e4)
 fig1.gca().set_ylabel(r'Electron density (cm^-3)')
 fig1.gca().set_xlabel('Universal time')
+
+fig3=plt.figure()
+
+plt.plot(times,ozhogin_density_latitude_factor(MLAT,InvLat))
+plt.xlabel('Universal time')
+plt.ylabel('Ozhogin latitude factor')
+
+fig3=plt.figure()
+
+#plt.plot(times,MLT)
+
+#plt.plot(times,sheeley_density_trough(Lstar,MLT),label='Normal')
+#plt.plot(times,124*(3.0/Lstar)**4.0,label='Without MLT dependence')
+plt.plot(times,(36*(3.0/Lstar)**3.5*np.cos((MLT-7.7*(3.0/Lstar)**2.0+12)*np.pi/12))/sheeley_density_trough(Lstar,MLT))
+plt.legend()
+plt.xlabel('Universal time')
+plt.ylabel('Sheeley trough density')
 
 plt.show()
