18598da4f3259377971b5c3c5c7769f644683d33
John D. Haiducek <jhaiduce@castiron.lanl.gov>
2014-06-27 16:14:27 -0600

    Changes to account for differences in version numbers.

--- emfisis.py
+++ emfisis.py
@@ -1,4 +1,5 @@
 from spacepy import pycdf
+import spacepy
 import numpy as np
 import os
 import datetime
@@ -7,7 +8,7 @@ from spacepy import datamodel as dm
 
 EMFISISDIR='/n/toaster/u/jhaiduce/data/EMFISIS'
 
-def get_filename(scname,date):
+def get_filename(scname,date,version='1.3.10'):
     """
     Determines the filename of the CDF file corresponding to the requested spacecraft and date
     """
@@ -18,7 +19,7 @@ def get_filename(scname,date):
     if scname.lower() in('rbspb','rbsp-b'):
         scname='rbsp-b'
 
-    filename='{scname:s}_density_emfisis_{year:d}{month:02d}{day:02d}_v1.3.10.cdf'.format(scname=scname.lower(),year=date.year,month=date.month,day=date.day)
+    filename='{scname:s}_density_emfisis_{year:d}{month:02d}{day:02d}_v{version:s}.cdf'.format(scname=scname.lower(),year=date.year,month=date.month,day=date.day,version=version)
     return os.path.join(EMFISISDIR,filename)
 
 def get_data(scname,varnames,dstart,dend):
@@ -27,7 +28,6 @@ def get_data(scname,varnames,dstart,dend):
     """
 
     ordinal_dates=range(dstart.toordinal(),dend.toordinal())
-
     
     try:
         _ = (varname for varname in varnames)
@@ -38,9 +38,13 @@ def get_data(scname,varnames,dstart,dend):
 
     for ordinal in ordinal_dates:
         date=datetime.datetime.fromordinal(ordinal)
-        filename=get_filename(scname,date)
-        #cdffile=pycdf.CDF(filename)
-        cdffile=dm.fromCDF(filename)
+        try:
+            filename=get_filename(scname,date)
+            cdffile=dm.fromCDF(filename)
+        except spacepy.pycdf.CDFError:
+            filename=get_filename(scname,date,version='1.3.8')
+            cdffile=dm.fromCDF(filename)
+            
         for i,varname in enumerate(varnames):
             cdfvar=cdffile[varname]
             arr=np.ma.masked_equal(np.array(cdfvar),cdfvar.attrs['FILLVAL'])
